aa003f5d1c5008b21bcb46fc4b705e390d88a870
Sanket Borse <88883406+sanketborse@users.noreply.github.com>
2025-11-27 15:57:49 +0530

    Refactor clean_text function with type hints
    Refactor clean_text function to include type hints and improve regex patterns.

--- utils.py
+++ utils.py
@@ -1,16 +1,28 @@
 import re
 
-def clean_text(text):
+
+def clean_text(text: str) -> str:
+    """
+    Basic cleaning of scraped HTML text:
+    - Remove HTML tags
+    - Remove URLs
+    - Remove special characters
+    - Normalize spaces
+    """
     # Remove HTML tags
-    text = re.sub(r'<[^>]*?>', '', text)
+    text = re.sub(r"<[^>]*?>", "", text)
     # Remove URLs
-    text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
-    # Remove special characters
-    text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
+    text = re.sub(
+        r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+        "",
+        text,
+    )
+    # Remove special characters except space
+    text = re.sub(r"[^a-zA-Z0-9 ]", " ", text)
     # Replace multiple spaces with a single space
-    text = re.sub(r'\s{2,}', ' ', text)
+    text = re.sub(r"\s{2,}", " ", text)
     # Trim leading and trailing whitespace
     text = text.strip()
-    # Remove extra whitespace
-    text = ' '.join(text.split())
+    # Final normalization
+    text = " ".join(text.split())
     return text
